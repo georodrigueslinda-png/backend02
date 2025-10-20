1e7e0e1200294e45643623b714a37eadef056e53
unasptrabalho <geovannarodriguesp@icloud.com>
2025-10-20 19:48:22 -0300

    Initial commit

---
+++ src/people.js
@@ -0,0 +1,21 @@
+const people = [
+    {
+        name: "Geovanna Rodrigues",
+        age: 17,
+        email: "geovannarodriguesp@icloud.com"
+    },
+       
+    {
+        name: "Raphael Brandão",
+        age: 17,
+        email: "raphaeldagigi@icloud.com"
+    },
+       
+    {
+        name: "Victoria Duque",
+        age: 17,
+        email: "vicduque@icloud.com"
+    }
+]
+
+export { people }
