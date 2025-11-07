28b5ba47c08db4958167e436f0b5e2ae22b57a70
unasptrabalho <geovannarodriguesp@icloud.com>
2025-11-06 21:47:21 -0300

    oi

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { request, response } from "express"
 import cors from "cors"
 import { people } from "./people.js"
 
@@ -6,9 +6,25 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 app.get("/", (request, response) => {
     response.json(people)
 })
+  
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, password } = request.body.user
+
+    console.log(`
+        Nome:${name},
+         E-mail:${email},
+          Idade:${age},
+           Senha:${password}
+        `)
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+})
+
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
