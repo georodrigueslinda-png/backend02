85eb3304cd6ee255634e5ef2b88cd2a44609ad63
unasptrabalho <geovannarodriguesp@icloud.com>
2025-11-13 18:40:57 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import express, { request, response } from "express"
 import cors from "cors"
 import mysql from "mysql2"
-import { people } from "./people.js"
-const {DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD} = process.env
+const { DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD } = process.env
 const app = express()
 const port = 3333
 
 app.use(cors())
 app.use(express.json())
 app.get("/", (request, response) => {
-    response.json(people)
+    const selectCommand =  "SELECT name, email, age FROM geovanna_rodrigues"
+
+    database.query(selectCommand, (error, users) => {
+        if (error){
+            console.log(error)
+            return
+        }
+
+        response.json(users)
+    })
+   
 })
   
 app.post("/cadastrar", (request, response) => {
@@ -36,9 +45,9 @@ app.listen(port, () => {
 })
 
 const database = mysql.createPool({
-    database: "DATABASE_NAME",
-    host: "DATABASE_HOST",
-    user: "DATABASE_USER",
-    password: "DATABASE_PASSWORD",
+    database: DATABASE_NAME,
+    host: DATABASE_HOST,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
     connectionLimit: 10
 })
